784a92b61bb9716fe6296b947471aa491b1fcf44
Izaak Miller <jmill165@login1.isaac.utk.edu>
2023-09-20 12:30:23 -0400

    Refactor install script, add dryrun

--- install.sh
+++ install.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
-set_suffix() {
-	echo "$(date +%s).bak"
+set -e
+
+declare dry_run
+
+
+run() {
+	local command=("$@")
+	if [[ $dry_run == true ]] ; then
+		echo "[DRYRUN] ${command[@]}"
+	else
+		"${command[@]}"
+	fi
 }
+
 
 backup_file() {
 	local file="$1"
@@ -10,33 +21,52 @@ backup_file() {
 	local backup="${file}-${suffix}"
 	if [[ -h "$file" ]] ; then
 		echo "${file} is a symlink to $(readlink $file)"
-		mv -v "$file" "$backup"
+		# mv -v "$file" "$backup"
+		run mv -iv "$file" "$backup"
 	elif [[ -f "$file" ]] ; then
 		echo "${file} is a file; back it up -> ${file}-${suffix}"
-		mv -v "$file" "$backup"
+		# mv -v "$file" "$backup"
+		run mv -iv "$file" "$backup"
 	elif [[ -d "$file" ]] ; then
 		echo "${file} is a directory; back it up -> ${file}-${suffix}"
-		mv -v "$file" "$backup"
-	else
-		echo "${file} does not exist yet"
+		# mv -v "$file" "$backup"
+		run mv -iv "$file" "$backup"
 	fi
 }
 
+
+link_file() {
+	run ln -s "$source_file" "$target"
+}
+
+
 main() {
-	local suffix=$(set_suffix)
+	while getopts 'n' opt; do
+		case "$opt" in
+			n) dry_run=true ;;
+			*) echo 'error in command line parsing' >&2
+			   exit 1
+		esac
+	done
+
+	local file
 	local source_file
 	local target
+
+	# Set the suffix here so all files will have the same suffix.
+	local suffix="$(date +%s).bak"
 
 	for file in dotfiles/* ; do
 		source_file="$(pwd -P)/${file}"
 		target="$HOME/.$(basename $file)"
 		echo "$file -> $target"
-		if [[ -a "$target" ]] ; then
-			# Target exists.
-			backup_file "$target" "$suffix"
-		fi
-		ln -s "$source_file" "$target"
+		backup_file "$target" "$suffix"
+		link_file "$file" "$target"
 	done
 }
 
-main
+
+main "$@"
+
+
+exit 0
